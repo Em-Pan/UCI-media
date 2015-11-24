660bd82368b8a8b112826078f071ba19b7a5c03c
Mau Pan <mau.mailbox@gmail.com>
2015-11-24 10:28:24 -0800

    pull request

--- SplitAndPopover/ArticlesTableViewController.swift
+++ SplitAndPopover/ArticlesTableViewController.swift
@@ -91,25 +91,37 @@ class ArticlesTableViewController: UITableViewController, XMLParserDelegate {
         let currentDictionary = xmlParser.arrParsedData[indexPath.row] as Dictionary<String, String>
         
         var htmlContent = ""
+        let blankImage = "https://s0.wp.com/i/blank.jpg"
         
         myCell.cellTitle.text = currentDictionary["title"]
         myCell.cellPubDate.text = currentDictionary["pubDate"]! + "d ago"
         // Configure the cell...
         
         // Get first image from article and insert in cell image
-        /*
+        
         let url = NSURL(string: currentDictionary["link"]!)
         do {
             htmlContent = try NSString(contentsOfURL: url!, encoding: NSUTF8StringEncoding) as String
         } catch {
             print("error")
         }
-        print(htmlContent)
+        //print(htmlContent)
+        /*
+        if imageSource != blankImage {
+            myCell.cellImage.set
+        }
         */
-        
         return myCell
     }
     
+    /*
+    func firstMatchIn(string: NSString!, atRangeIndex: Int!) -> String {
+        var error : NSError?
+        let re = NSRegularExpression(pattern: self, options: .CaseInsensitive)
+        let match = re.firstMatchInString(string, options: .WithoutAnchoringBounds, range: NSMakeRange(0, string.length))
+        return string.substringWithRange(match.rangeAtIndex(atRangeIndex))
+    }
+    */
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let currentDictionary = xmlParser.arrParsedData[indexPath.row] as Dictionary<String, String>
